f7db800e6fe6d7f92059826a0052f7ffc43a505c
Mohamed Linaf <linaf7@gmail.com>
2025-12-10 12:43:13 +0500

    Enhance appraisal form actions; add conditional visibility for delete and assign to staff actions

--- app/Filament/Staff/Resources/AppraisalFormAssignedToStaffResource/Pages/EditAppraisalFormAssignedToStaff.php
+++ app/Filament/Staff/Resources/AppraisalFormAssignedToStaffResource/Pages/EditAppraisalFormAssignedToStaff.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Staff\Resources\AppraisalFormAssignedToStaffResource\Pages;
 
 use App\Filament\Staff\Resources\AppraisalFormAssignedToStaffResource;
+use App\Services\Shortcuts;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -13,7 +14,17 @@ class EditAppraisalFormAssignedToStaff extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->visible( in_array('HR', Shortcuts::callgetapi('/user/roles', ['id' => auth('staff')->user()->api_id])->json() ?? [])),
+            Actions\Action::make('Assigntostaff')
+                ->label('Complete and Send to Staff')
+                ->button()
+                ->color('success')
+                ->visible(fn($record) => $record->status === \App\Enum\AssignedFormStatus::PendingQuestionnaireEdit )
+                ->action(fn ($record) => $record->update([
+                    'status' => \App\Enum\AssignedFormStatus::PendingStaff->value,
+                    'assigned_date' => now(),
+                ])),
         ];
     }
 }
